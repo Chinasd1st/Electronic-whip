ce76c0350d48d66b71f50e1189fbf33b11ebb948
Chinasd1st <1297537384@qq.com>
2023-12-15 20:43:07 +0800

    添加项目文件。

---
+++ 鸿合智能电子教鞭/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 鸿合智能电子教鞭
+{
+    public partial class Form1 : Form
+    {
+        private string localFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            SoundPlayer player = new SoundPlayer(localFilePath);
+            player.Play();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            localFilePath = null;
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Multiselect = false,
+                Title = "选择教鞭声音文件",
+                Filter = ".wav|*.wav*"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                localFilePath = ofd.FileName;//返回文件的完整路径
+                MessageBox.Show("音频文件已成功加载！");
+            }
+            button1.Enabled = !string.IsNullOrEmpty(localFilePath);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
